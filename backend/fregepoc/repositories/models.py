cb9f11318ba8b8fa15733f4feea824aafd074229
vladfedoriuk <vlad.fedoriuk2000@gmail.com>
2022-03-18 00:40:00 +0100

    Add a Repository app

---
+++ backend/fregepoc/repositories/models.py
@@ -0,0 +1,32 @@
+import uuid
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Repository(models.Model):
+    id = models.UUIDField(_("ID"), primary_key=True, default=uuid.uuid4, editable=False)
+    git_url = models.URLField(
+        _("git url"), help_text=_("The url used to clone the repository")
+    )
+    repo_url = models.URLField(
+        _("repo url"), help_text=_("The url used to crawl or visit the repository.")
+    )
+    commit_hash = models.CharField(
+        max_length=40,
+        verbose_name=_("commit hash"),
+        help_text=_("The commit hash actual at the time of crawling."),
+    )
+    crawl_time = models.DateTimeField(
+        _("crawl time"),
+        auto_now_add=True,
+        help_text=_(
+            "The time when the repository has got discovered. It is set automatically on creation."
+        ),
+    )
+    download_time = models.DateTimeField(
+        _("download time"),
+        blank=True,
+        null=True,
+        help_text=_("The time when the repository has got downloaded."),
+    )
--- backend/fregepoc/settings/dev.py
+++ backend/fregepoc/settings/dev.py
@@ -4,22 +4,23 @@ import os
 
 # DATABASES
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DJANGO_DATABASE_NAME', 'frege'),
-        'USER':  os.environ.get('DJANGO_DATABASE_USER', 'frege'),
-        'PASSWORD': os.environ.get('DJANGO_DATABASE_PASSWORD', 'admin'),
-        'HOST': os.environ.get('DJANGO_DATABASE_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DJANGO_DATABASE_PORT', '15432'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DJANGO_DATABASE_NAME", "frege"),
+        "USER": os.environ.get("DJANGO_DATABASE_USER", "frege"),
+        "PASSWORD": os.environ.get("DJANGO_DATABASE_PASSWORD", "admin"),
+        "HOST": os.environ.get("DJANGO_DATABASE_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("DJANGO_DATABASE_PORT", "15432"),
     }
 }
 
 # CELERY STUFF
-CELERY_BROKER_URL = f"redis://{os.environ.get('DJANGO_REDIS_HOST', '127.0.0.1')}:" + \
-                    f"{os.environ.get('DJANGO_REDIS_PORT', '16379')}"
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
+CELERY_BROKER_URL = (
+    f"redis://{os.environ.get('DJANGO_REDIS_HOST', '127.0.0.1')}:"
+    + f"{os.environ.get('DJANGO_REDIS_PORT', '16379')}"
+)
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
 
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_CACHE_BACKEND = 'django-cache'
-
+CELERY_RESULT_BACKEND = "django-db"
+CELERY_CACHE_BACKEND = "django-cache"
