fd645b6e8c263d4dd187daa2b4af1d19607d6026
vladfedoriuk <vlad.fedoriuk2000@gmail.com>
2022-04-12 22:46:03 +0200

    fix: replace try...except...pass with suppress

--- backend/fregepoc/indexers/admin.py
+++ backend/fregepoc/indexers/admin.py
@@ -1,3 +1,5 @@
+import contextlib
+
 from django.contrib import admin
 
 from fregepoc.indexers.base import indexers
@@ -7,7 +9,5 @@ from fregepoc.utils.admin import AutoModelAdmin
 # https://medium.com/hackernoon/automatically-register-all-models-in-django-admin-django-tips-481382cf75e5
 
 for indexer_model in indexers:
-    try:
+    with contextlib.suppress(admin.sites.AlreadyRegistered):
         admin.site.register(indexer_model, AutoModelAdmin)
-    except admin.sites.AlreadyRegistered:
-        pass
