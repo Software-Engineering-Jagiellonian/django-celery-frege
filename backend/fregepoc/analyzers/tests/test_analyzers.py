08dc745dfafa8fa9fbc805f304bcda7112385485
kamilsuchy <44306803+kamilsuchy@users.noreply.github.com>
2022-06-01 20:37:00 +0200

    fix:fix extension

--- backend/fregepoc/analyzers/tests/test_analyzers.py
+++ backend/fregepoc/analyzers/tests/test_analyzers.py
@@ -18,7 +18,7 @@ class TestAnalyzers:
             ),
             (
                 ProgrammingLanguages.JAVA,
-                {"repo_relative_file_path": "binary_tree.php"},
+                {"repo_relative_file_path": "binary_tree.java"},
             ),
             (
                 ProgrammingLanguages.PHP,
