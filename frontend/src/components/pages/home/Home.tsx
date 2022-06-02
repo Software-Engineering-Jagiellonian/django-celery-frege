59380570112eb95806b9f0450b39c9a984216c4f
JakGad <44136683+JakGad@users.noreply.github.com>
2022-06-02 12:21:58 +0200

    feat:[FREGE-173] Create widgets hub (#6)

--- frontend/src/components/pages/home/Home.tsx
+++ frontend/src/components/pages/home/Home.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { WidgetsHub } from '../../widgets/hub/WidgetsHub';
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <WidgetsHub
+        widgets={[
+          {
+            widgetId: '2',
+            dashboardId: 'RRj1M49nz',
+            widgetName: 'widget2',
+            dashboardName: 'dashboard'
+          },
+          {
+            widgetId: '4',
+            dashboardId: 'RRj1M49nz',
+            widgetName: 'widget2',
+            dashboardName: 'dashboard'
+          }
+        ]}
+        hubId={'general/RRj1M49nz'}
+      />
+    </div>
+  );
 }
 
 export default Home;
--- frontend/src/components/topBar/TopBar.tsx
+++ frontend/src/components/topBar/TopBar.tsx
@@ -1,19 +1,18 @@
 import React, { FC } from 'react';
 import styles from './navbar.module.scss';
 import { MenuAppFill } from 'react-bootstrap-icons';
+import { StyledButton } from '../styledButton/StyledButton';
 
 interface TopBarProps {
   onMenuClick: () => void;
 }
 
-const Topbar: FC<TopBarProps> = ({ onMenuClick }) => {
+export const TopBar: FC<TopBarProps> = ({ onMenuClick }) => {
   return (
     <div className={styles.navbar}>
-      <button className={styles.menuButton} onClick={onMenuClick}>
+      <StyledButton className={styles.menuButton} onClick={onMenuClick}>
         <MenuAppFill />
-      </button>
+      </StyledButton>
     </div>
   );
 };
-
-export default Topbar;
