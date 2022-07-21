50db43a42eba4b7b586cec057bd06270a27934ac
ConsoleRunner <wojciechly@wp.pl>
2022-07-21 03:15:17 +0200

    feat: configured reverse-proxy to work with the ad-hoc frontend implementation

--- backend/fregepoc/settings.py
+++ backend/fregepoc/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "api_static/"
 STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
