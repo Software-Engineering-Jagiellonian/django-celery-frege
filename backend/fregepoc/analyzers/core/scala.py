4d9dd76aa2c56b1ed6b443179edd051e3f9f00d1
wszlosek <wojciech185sz@gmail.com>
2022-12-08 19:35:58 +0100

    Add new metrics for Scala

--- backend/fregepoc/analyzers/core/scala.py
+++ backend/fregepoc/analyzers/core/scala.py
@@ -10,10 +10,12 @@ from fregepoc.repositories.utils.analyzers import repo_file_content
 class ScalaFileAnalysisResult(TypedDict):
     lines_of_code: int
     token_count: int
-    average_lines_of_code: int
-    average_token_count: int
-    average_cyclomatic_complexity: int
-    average_parameter_count: int
+    average_lines_of_code: float
+    average_token_count: float
+    average_cyclomatic_complexity: float
+    function_count: int
+    average_function_name_length: float
+    average_parameter_count: float
 
 
 @AnalyzerFactory.register(ProgrammingLanguages.SCALA)
@@ -24,12 +26,18 @@ class ScalaAnalyzer(BaseAnalyzer[ScalaFileAnalysisResult]):
                 ".scala", file_content
             )
 
+            def average_func_name_len(function_list):
+                func_name_lengths = [len(func.name) for func in function_list]
+                return sum(func_name_lengths) / len(func_name_lengths) if func_name_lengths else 0.0
+
             return {
                 "lines_of_code": analysis_result.nloc,
                 "token_count": analysis_result.token_count,
                 "average_lines_of_code": analysis_result.average_nloc,
                 "average_token_count": analysis_result.average_token_count,
                 "average_cyclomatic_complexity": analysis_result.average_cyclomatic_complexity,
+                "function_count": len(analysis_result.function_list),
+                "average_function_name_length": average_func_name_len(analysis_result.function_list),
                 "average_parameter_count": analysis_result.functions_average(
                     "parameter_count"
                 ),
