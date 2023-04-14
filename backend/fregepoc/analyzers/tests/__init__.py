c8bf0da3a81571a9131a273f65da08c4b4b40abd
Andrzej Radzik <andrzej.radzik@onet.pl>
2023-04-14 13:51:37 +0200

    feat: add docstrings to some files
    add docstrings to backend/fregepoc/analyzers/core/generic.py
    and backend/fregepoc/analyzers/tests/__init__.py

---
+++ backend/fregepoc/analyzers/tests/__init__.py
@@ -0,0 +1,13 @@
+"""
+Package for testing analyzers
+
+Most files are stored in "generic" submodule.
+File "generic.util.generic_test_util.py" constants function
+"generic_test" which compares analyzer results to the values
+precomputed for each language.
+
+Other submodules in generic submodule contain files necessary for
+testing analyzer of language name coresponding to submodule name.
+These files are constants.py, test_<language_name>.py and __mock__
+with example code to analyze.
+"""
