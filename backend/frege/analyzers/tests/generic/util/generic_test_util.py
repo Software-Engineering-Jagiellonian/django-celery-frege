0a6647dde0d0acd94f28c352d7b94b03e7a6433e
Renis-1 <jabystrowski@gmail.com>
2025-03-24 19:17:22 +0100

    fix: even better mocks for analyzer tests

--- backend/frege/analyzers/tests/generic/util/generic_test_util.py
+++ backend/frege/analyzers/tests/generic/util/generic_test_util.py
@@ -1,9 +1,10 @@
 import pytest
 
-from unittest.mock import patch
+from unittest.mock import patch, mock_open
 
 from frege.analyzers.core import AnalyzerFactory
 from frege.repositories.factories import RepositoryFileFactory
+
 
 
 def generic_test(
@@ -23,13 +24,14 @@ def generic_test(
         language=programming_language,
         **repo_file_params,
     )
-    for analyzer in analyzers:
-        with patch("lizard.analyze_file.analyze_source_code", return_value=mock_lizard_result):
-            actual = analyzer.analyze(repo_file)
-        for (param, expected) in zip(parameter_types, expected_values):
-            error_message = (
-                f"For {param} actual: {actual[param]} not equal to expected: {expected}"
-            )
-            assert actual[param] == pytest.approx(
-                expected, 0.01
-            ), error_message
+    with patch("frege.analyzers.core.generic.repo_file_content", new_callable=mock_open, read_data="def foo(): pass"):
+        for analyzer in analyzers:
+            with patch("lizard.analyze_file.analyze_source_code", return_value=mock_lizard_result):
+                actual = analyzer.analyze(repo_file)
+                for (param, expected) in zip(parameter_types, expected_values):
+                    error_message = (
+                        f"For {param} actual: {actual[param]} not equal to expected: {expected}"
+                    )
+                    assert actual[param] == pytest.approx(
+                        expected, 0.01
+                    ), error_message
