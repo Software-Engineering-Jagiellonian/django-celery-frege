8f07a88ec65d429ff8bf963ef848eba8149b8d46
Gochad <malgorzata.drag@spoton.com>
2025-04-03 20:22:56 +0200

    remove unused deps

--- backend/frege/repositories/tests/test_tasks.py
+++ backend/frege/repositories/tests/test_tasks.py
@@ -1,6 +1,5 @@
 from unittest.mock import call
 
-import git
 import pytest
 from pytest_mock import MockerFixture
 
@@ -8,7 +7,6 @@ from frege.repositories.models import RepositoryFile
 from frege.repositories.tasks import process_repo_task
 from frege.repositories.utils.paths import get_repo_local_path
 from frege.repositories.utils.tests import MOCK_DOWNLOAD_PATH
-from frege.repositories.exceptions import DownloadDirectoryFullException
 
 
 @pytest.mark.django_db
