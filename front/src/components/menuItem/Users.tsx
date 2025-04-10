ea736820a645c6ecafb9851e0b8d992f946270c7
Agnieszka Trojanowska <agnieszka.t0544@gmail.com>
2025-04-10 23:31:30 +0200

    feat: add navigation to users

--- front/src/components/menuItem/Users.tsx
+++ front/src/components/menuItem/Users.tsx
@@ -1,24 +1,18 @@
-import React, { FC, useState, MouseEvent } from 'react';
+import React from 'react';
 import { PeopleFill } from 'react-bootstrap-icons';
-// import { useNavigate } from 'react-router-dom';
 import styles from './MenuItem.module.scss';
+import Link from 'next/link';
 
 const Users = () => {
-  // const navigate = useNavigate();
-
-  // const onMenuItemClick = (e: { stopPropagation: () => void }) => {
-  //   navigate('/users');
-  //   e.stopPropagation();
-  // };
-
   return (
-    // <div className={styles.menuItem} onClick={onMenuItemClick}>
-    <div className={styles.menuItem}>
-      <div className={styles.iconContainer}>
-        <PeopleFill />
+    <Link href="/users">
+      <div className={styles.menuItem}>
+        <div className={styles.iconContainer}>
+          <PeopleFill />
+        </div>
+        <div className={styles.label}>All users</div>
       </div>
-      <div className={styles.label}>All users</div>
-    </div>
+    </Link>
   );
 };
 
