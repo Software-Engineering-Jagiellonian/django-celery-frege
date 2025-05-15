0c106de87cda19b9685ccf90a34f4fd69275bcfd
Agnieszka Trojanowska <agnieszka.t0544@gmail.com>
2025-05-15 14:24:53 +0200

    chore: configure eslint

--- front/eslint.config.mjs
+++ front/eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+  }
+})
+
 ];
 
 export default eslintConfig;
